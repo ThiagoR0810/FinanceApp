ed50c06311b80dee911008a1bf9bdf5f84a4638a
Thiago Corrêa <thiago.ribeiroc.0810@gmail.com>
2025-03-06 15:46:26 -0300

    creating a Home page and configuring its route

--- frontend/src/contexts/auth.js
+++ frontend/src/contexts/auth.js
@@ -32,7 +32,7 @@ function AuthProvider({ children }) {
   }
 
   return (
-    <AuthContext.Provider value={{ user, signUp, loadingAuth }}>
+    <AuthContext.Provider value={{ signed: !!user, user, signUp, loadingAuth }}>
       {children}
     </AuthContext.Provider>
   );
