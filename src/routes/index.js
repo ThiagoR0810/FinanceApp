590a33bebd3d6320b0c00bc99c2ac255272de1b0
Thiago Corrêa <thiago.ribeiroc.0810@gmail.com>
2025-03-06 10:25:36 -0300

    configuring SignIn and SignUp routes

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import AuthRoutes from "./auth.routes";
+
+function Routes() {
+  const loading = false;
+  const signed = false;
+  
+  return (
+    signed ? <View></View> : <AuthRoutes/>
+  );
+}
+
+export default Routes;
