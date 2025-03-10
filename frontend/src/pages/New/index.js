6b0072b12515f8c4ffa7ce4a3ddcf963941612d0
Thiago Corrêa <thiago.ribeiroc.0810@gmail.com>
2025-03-10 15:51:04 -0300

    add RegisterTypes component for selecting income and expense types

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -4,6 +4,7 @@ import { Background, Input, SubmitButton, SubmitText } from "./styles";
 import { SafeAreaView, TouchableWithoutFeedback, Keyboard } from "react-native";
 
 import Header from "../../components/Header";
+import RegisterTypes from "../../components/RegisterTypes";
 
 export default function New() {
   const [labelInput, setLabelInput] = useState('');
@@ -28,6 +29,8 @@ export default function New() {
             value={valueInput}
             onChangeText={ (text) => setValueInput(text) }
           />
+
+          <RegisterTypes type={type} sendTypeChanged={ (item) => setType(item) } />
 
           <SubmitButton>
             <SubmitText>Registrar</SubmitText>
