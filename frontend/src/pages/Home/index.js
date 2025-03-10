7ec4ed8c3f4df9df7714f6e9b3ac224c7f43f345
Thiago Corrêa <thiago.ribeiroc.0810@gmail.com>
2025-03-10 14:07:34 -0300

    add balance display component and integrate into Home screen

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -4,12 +4,16 @@ import { View, Text, Button } from "react-native";
 import { AuthContext } from "../../contexts/auth";
 
 import Header from "../../components/Header";
-import { Background } from "./styles";
+import { Background, ListBalance } from "./styles";
 
 import api from "../../services/api";
 import { format } from "date-fns";
 
+import { useIsFocused } from "@react-navigation/native";
+import BalanceItem from "../../components/BalanceItem";
+
 export default function Home() {
+  const isFocused = useIsFocused();
   const [listBalance, setListBalance] = useState([]);
 
   const [dateMovements, setDateMovements] = useState(new Date());
@@ -34,10 +38,19 @@ export default function Home() {
     getMovements();
 
     return () => isActive = false;
-  }, [] )
+  }, [isFocused] )
+
   return (
     <Background>
       <Header title="Minhas movimentações" />
+
+      <ListBalance 
+        data={listBalance}
+        horizontal={true}
+        showHorizontalScrollIndicator={false}
+        keyExtractor={ item => item.tag }
+        renderItem={ ({ item }) => ( <BalanceItem data={item} /> ) }
+      />
     </Background>
   );
 }
