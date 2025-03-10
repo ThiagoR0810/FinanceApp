c5e74f15ef6f7ab2a16e63510cdb563a0656c8e4
Thiago Corrêa <thiago.ribeiroc.0810@gmail.com>
2025-03-10 14:38:34 -0300

    add New page for record registration and integrate into navigation

--- frontend/src/routes/app.routes.js
+++ frontend/src/routes/app.routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import Home from "../pages/Home";
+import New from "../pages/New";
 
 const AppDrawer = createDrawerNavigator();
 
@@ -26,6 +27,11 @@ function AppRoutes() {
 			<AppDrawer.Screen 
 				name="Home" 
 				component={Home}
+			/>
+
+			<AppDrawer.Screen 
+				name="Registrar" 
+				component={New}
 			/>
 		</AppDrawer.Navigator>
 	);
